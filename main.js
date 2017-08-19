feb3c11624fbba508d4adca8632c94668e5d943e
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-08-18 22:01:10 -0500

    trying to get delete button to work

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var createCard = document.getElementById('createCard');
 var newCardName = document.getElementById('newCardName');
 var addTheDamnCard = document.getElementById('addTheDamnCard');
 var cardIDnumber = 0;
+var deleteButton = document.getElementById('deleteButton');
 createCard.addEventListener("click", function() {
 	
 	domString();	
@@ -19,7 +20,7 @@ function domString() {
 	var newCard ="";
 	newCard += `<div id="card_${cardIDnumber}"class="cardAdd">`;
 	newCard += `<h1>` + newCardName.value + `</h1>`;
-	newCard += `<input id="deleteButton" type="submit" value="Delete" onclick="deleteCard()">`;
+	newCard += `<input id="deleteButton" type="submit" value="Delete">`;
 	newCard += `</div>`;
 	cardString += newCard;
 	cardIDnumber++;
@@ -31,15 +32,21 @@ function domString() {
 
 function writeToDom(strang) {
 	addTheDamnCard.innerHTML += strang;
+}
 
+function addListenersToDeleteButton() {
+	var delButtonId = `card_${cardIDnumber}`; 
+	document.getElementById(delButtonId).addEventListener('click', function(e){ deleteCard(e)});
 
-
-	
 }
 
 //work from the button to its parent (div container you want to delete.  then the main div and delete the specific chile )
-function deleteCard() {
-	var elem = document.getElementById(cardAdd);
-	elem.parentNode.removeChild(elem);
+function deleteCard(e) {
+	var divNode = e.currentTarget.parentNode;
+	var mainDivParent = document.getElementById("card_${cardIDnumber}");
+	mainDivParent = removeChild(divNode);
+
+	// var elem = document.getElementById($cardIDnumber);
+	// elem.parentNode.removeChild(elem);
 }
 
